a3e2eaa343baf961cd111e1c97ef3f3ad8c91d7a
crusselsh <crs.hunter@gmail.com>
2015-10-13 21:30:47 -0400

    strip down download script to just download and save

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ miscellaneous scripts at CUSP data facility, so far includes:
 
 1. "download-lehd.py" - which get's a URL's full table list and iterates through to:
   * save data to local directory
-  * upload data to sftp given input credentials
-  * remove data from local directory
 2. "dlnoad-parse-add-lehd.py" - also gets full list of "...csv.gz" files from site and:
   * save data to local directory
   * add data to master file (<dataset>_<st>.csv)
--- download-lehd.py
+++ download-lehd.py
@@ -3,25 +3,14 @@
     Date: Oct 7, 2015
     
     usage:
-    python download-test.py <url-to-data-files> <sftp creds>
+    python download-test.py <url-to-data-files>
     
     details:
-    program get's a full list of "...".gz files from an input URL contained in any <a href=""> html element, then iterates through the list to:
-        a) download the .gz file,
-        b) upload file to sftp site (given in <sftp creds> argument), and
-        c) removes file from local directory
-        
-    Note: for sftp credentals (<sftp creds>), can either 
-        a) link to a saved JSON file with line 45 or 
-        b) paste/type in JSON string directly to command line using line 46
+    program get's a full list of "...".gz files from an input URL contained in any <a href=""> html element, then iterates through the list to download and save the .gz file
 '''
-import os
 import sys
 import requests
 from bs4 import BeautifulSoup
-import pysftp
-import json
-import csv, gzip, io
 
 # http://lehd.ces.census.gov/data/lodes/LODES7/ny/rac/
 
@@ -43,12 +32,11 @@ def download_file(url, fname):
         for chunk in r.iter_content(1000):
             fd.write(chunk)
     print 'file written to local'
-    # b) call sftp transfer function and c) delete local file
 
 if __name__=='__main__':
     files_url = sys.argv[1] # input "http://path/to/data/" at command line
-    print files_url
-    # get all file names
+    
+        # get all file names
     all_files = get_all_files(files_url)
     print 'Files found: ', len(all_files)
     
